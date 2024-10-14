55c72ecd72e46a85f2f44f3239f3e7093ac3ebca
DBin_K <DBinKv1@Gmail.com>
2024-10-14 23:37:19 +0800

    Update ESP32C3 Bluetooth.py
    need to encoding string

--- ESP32C3 Bluetooth.py
+++ ESP32C3 Bluetooth.py
@@ -64,7 +64,7 @@ class BLE():
 
     def advertise(self):
         name = bytes(self.name, 'UTF-8')
-        self.ble.gap_advertise(100, bytearray('\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name)
+        self.ble.gap_advertise(100, bytearray(b'\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name)
 
 
 ble = BLE("ESP32 Echo")
